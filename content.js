c7b9555e78117f1cbbcfbf98ee863796b8a82053
Ethan-Cruse <ethanhcruse@gmail.com>
2022-11-17 10:55:35 +1000

    Removed Event listener "click"

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ const injection = timeJoin;
 
 
 
-function makingURL () {              // function forming object with number from 9-21 and combining into injection html text.
+function makingURL () {         // function forming object with number from 9-21 and combining into injection html text.
     var result = [];
     for (var i=9; i<22; i++) {
         var time = i;
@@ -19,22 +19,12 @@ function makingURL () {              // function forming object with number from
     return result;
 }
 
-const injectGuide = function(){                                      // Injection replaceing text in sub-menu with links.
+const injectGuide = function(){                 // Injection replaceing text in sub-menu with links.
     guide.innerHTML = injection;
     guide.classList.add("timeButton")
 }
 
 injectGuide();
-
-                                //event listen to refresh page on click 
-
-// guide.addEventListener("click",clicked)
-
-//     function clicked(){
-//     // window.location.reload();
-//     history.pushState({}, null, newUrl);
-//     }
-
 
 
 
